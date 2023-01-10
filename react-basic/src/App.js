9bb5b5e38e900c0ba589d04f1b9c8f0aa3d6d07b
sm-amoled <moktak072@naver.com>
2023-01-10 20:37:17 +0900

    #5.2 React useEffect

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,12 +1,35 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  const iRunOnlyOnce = () => {
+    console.log("Call Only Once");
+  };
+  useEffect(iRunOnlyOnce, []);
+  // console.log("Search For", keyword);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search For", keyword);
+    }
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Hello JS World</h1>
-      <Button text={"Continue"} />
-      <button>stop</button>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here"
+      />
+
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Count Up</button>
     </div>
   );
 }
