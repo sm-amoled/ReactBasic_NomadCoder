703ed73f68983bb615c59d104af59056d60c3cdc
sm-amoled <moktak072@naver.com>
2023-01-10 22:25:49 +0900

    #6.1 state with array

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,34 +1,42 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [isShowing, setIsShowing] = useState(true);
-  const onClick = () => {
-    setIsShowing((current) => !current);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => {
+    setTodo(event.target.value);
   };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
 
   return (
     <div>
-      <button onClick={onClick}>{isShowing ? "hide" : "show"}</button>
-      <Coffee isShowing={isShowing} />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write Your To-Do"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {todos.map((element, index) => (
+        <li key={index}>{element}</li>
+      ))}
     </div>
   );
-}
-
-function Coffee(isShowing) {
-  function created() {
-    console.log("created");
-    return destroyed;
-  }
-  function destroyed() {
-    console.log("destroyed");
-  }
-
-  useEffect(() => {
-    console.log("Coffee is here");
-    return () => console.log("Coffee isn't here");
-  }, [isShowing]);
-
-  return <h1>{isShowing ? "show" : "hide"}</h1>;
 }
 
 export default App;
